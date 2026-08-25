20ad16df075ecf731e0aeec89c0da9541661ffec
amanguptacloud <amanguptashiina@gmail.com>
2026-08-25 23:48:34 +0530

    ques 2 done, type detection

---
+++ chapter 2/2.2.py
@@ -0,0 +1,7 @@
+# 2. Type detective  [Required]
+# Task: Ask the user for a value, print its type before conversion,
+#       then convert it to an integer and print the new type.
+a=input("enter value: \n")
+print(type(a))
+a=int(a)
+print(type(a))
