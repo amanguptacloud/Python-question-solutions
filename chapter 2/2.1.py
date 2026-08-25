c6ffddaf3cb41ab4e27d7a21518e0d82aa1f7934
amanguptacloud <amanguptashiina@gmail.com>
2026-08-25 23:41:25 +0530

    ques 2 done, calculation of 2 numbers

---
+++ chapter 2/2.1.py
@@ -0,0 +1,11 @@
+# 1. Two-number calculator  
+# Take two numbers from the user and print their sum, difference, product, quotient, and remainder.
+
+a=int(input("give 1st number: \n"))
+b=int(input("give 2nd number: \n"))
+
+print("sum: ",a+b)
+print("difference: ",a-b)
+print("product: ",a*b)
+print("quotient: ",a/b)
+print("remainder: ",a%b)
