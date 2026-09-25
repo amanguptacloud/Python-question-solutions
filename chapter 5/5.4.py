1d5484549f7a13f4a301b08a79f8f093c78b9404
amanguptacloud <amanguptashiina@gmail.com>
2026-09-25 23:29:54 +0530

    ques 5.4 done, set comparison tool

---
+++ chapter 5/5.4.py
@@ -0,0 +1,10 @@
+# 4. Set comparison tool   
+# Task: Given two sets of numbers, print their union and
+# intersection and explain in comments what each result means.
+
+a={3,5,4,2,55,73,79}
+b={5,33,62,6,1,73,2}
+c=a.union(b) #combines both set
+print(c)
+c=a.intersection(b) #keeps only common elements
+print(c) 
